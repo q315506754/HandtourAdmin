49f95666db1230606f30c8b3955844de7e1296dd
zhawei <zhawei@bingdian.com>
2016-07-15 17:03:36 +0800

    框架搭建

---
+++ src/main/java/com/handtours/controller/IndexController.java
@@ -0,0 +1,7 @@
+package com.handtours.controller;
+
+/**
+ * Created by april on 2016/7/15.
+ */
+public class IndexController {
+}
---
+++ src/test/java/com/handtours/impl/ProvinceHandlerTest.java
@@ -0,0 +1,37 @@
+package com.handtours.impl;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhawei
+ */
+public class ProvinceHandlerTest {
+
+
+    public void testFindOne() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+    }
+
+    public void testSave() throws Exception {
+
+    }
+
+    public void testUpdate() throws Exception {
+
+    }
+
+    public void testDelete() throws Exception {
+
+    }
+
+    public void testSearch() throws Exception {
+
+    }
+}
